527de6cfab3835b25832f5f2e5ee7c9340ce9877
yashika_agrawal_pw11_021 <yashikaagrawal23@gamil.com>
2023-09-24 09:48:09 +0530

    p

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-gray-800 text-sm p-4 text-center text-white">
-      Copyright © 2023 Yogesh Sharma. All Rights reserved.
+      Copyright © 2023 Yashika Agrawal. All Rights reserved.
     </div>
   );
 };
